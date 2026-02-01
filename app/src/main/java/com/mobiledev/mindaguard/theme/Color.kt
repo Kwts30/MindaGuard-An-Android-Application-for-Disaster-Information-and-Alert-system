fe9bbf641b70d2abd6cc93f1ea3bd00578a95997
Jaryl Kurt Benedicto <121588490+Kwts30@users.noreply.github.com>
2026-02-02 01:03:35 +0800

    Lists of Updates:
    -full mainpage ui
    -Foundation UI for: map,main menu, energency, and alert.
    - bug fixes on the loading screen (Splash Screen) to the main page
    - adding time specifics for the morning,afternoon, and night images on the mainpage.

---
+++ app/src/main/java/com/mobiledev/mindaguard/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.mobiledev.mindaguard.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// LIGHT THEME COLORS
+val LightBackground = Color(0xFFF4F5F9)
+val LightSurface = Color(0xFFFFFFFF)
+val LightPrimary = Color(0xFF0D62FF)   // blue accent
+val LightSecondary = Color(0xFFFF7043) // orange accent
+val LightOnBackground = Color(0xFF1C1E21)
+val LightOnSurface = Color(0xFF222222)
+val LightMuted = Color(0xFF9E9E9E)
+
+// DARK THEME COLORS
+val DarkBackground = Color(0xFF0C0E14)
+val DarkSurface = Color(0xFF181C24)
+val DarkPrimary = Color(0xFF4D8FFF)
+val DarkSecondary = Color(0xFFFF8A65)
+val DarkOnBackground = Color(0xFFF5F5F5)
+val DarkOnSurface = Color(0xFFE0E0E0)
+val DarkMuted = Color(0xFF9FA4B2)
+val mindaguardGreen = Color(0xFF2C4E3D)
